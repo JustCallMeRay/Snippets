e4e952138fe49a6f6bebbc3f10132711e3bcac5c
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2022-08-10 17:57:27 +0100

    A lot
    added new "per term" scoring system, and reward function, which massively reduces training time
    added a "bad seed" detector which resets the alogrithm if it isn't working and spirals in the wrong direction
    also improved perfromace, readablity, scaleabilty
    added comment to better explain tanh vs tansig choice

--- MachineLearningCtoF.py
+++ MachineLearningCtoF.py
@@ -3,225 +3,29 @@
   # the model is trained via a genetic method 
     # scores get worse untill generation 10 - 15, im not sure why, 
     # I assume its something to do with the scoring as the moving average arrays are populated during those generations
-  
-  
 
 import numpy as np
+from training import TRAINING_DATA  #not inlcuded in repo, ~= 200 pieces
 
-NUM_CHILDREN = 3  
-generation = 0  # for debugging and visualisation only
-scores_moving_avg = [3854147.4] * 10  #for debugging, high number is an average score after initilisation, avoids errors
-stag_avg = [-1] * 15  #estimates stagnation (if a local minima(/maxima) is found we can try to step out of it. 
-CtoF = []  #best scores from each generation, for use in end program.
-SPECIMINS_PER_GEN = 90  #const, 
+#SCORING_METHODS = ("PER_TERM", "RMS", "WORST_GUESS") #was here to act as an enum
+SCORE_METHOD = "PER_TERM"
+NUM_CHILDREN = 3  #const
+generation = 0  # for debugging
+scores_moving_avg = [3854147.4] * 10  #for debugging, high number is average score after initilisation
+stag_avg = [-1] * 15  #estimates stagnation
+CtoF = []  #best scores from each generation
+SPECIMINS_PER_GEN = 90  #const
 genetic_pool = [] #global for convience and defaults
 df = 15  #dision factor larger value results in smaller changes.
 delta_score_avg = [-1] * 10
 
-#Training data included here for ease of reading, created via script 
-  # Data corelates to celcius to fareinheit conversions
-TRAINING_DATA = [
-(8, 46.4),
-(86, 186.8),
-(49, 120.2),
-(92, 197.6),
-(87, 188.6),
-(45, 113.0),
-(75, 167.0),
-(57, 134.60000000000002),
-(21, 69.80000000000001),
-(65, 149.0),
-(36, 96.8),
-(86, 186.8),
-(61, 141.8),
-(37, 98.60000000000001),
-(65, 149.0),
-(3, 37.4),
-(52, 125.60000000000001),
-(81, 177.8),
-(32, 89.6),
-(35, 95.0),
-(57, 134.60000000000002),
-(69, 156.2),
-(72, 161.6),
-(56, 132.8),
-(81, 177.8),
-(23, 73.4),
-(11, 51.8),
-(43, 109.4),
-(33, 91.4),
-(53, 127.4),
-(92, 197.6),
-(1, 33.8),
-(75, 167.0),
-(33, 91.4),
-(28, 82.4),
-(64, 147.2),
-(57, 134.60000000000002),
-(40, 104.0),
-(80, 176.0),
-(90, 194.0),
-(46, 114.8),
-(28, 82.4),
-(87, 188.6),
-(58, 136.4),
-(43, 109.4),
-(91, 195.8),
-(97, 206.6),
-(65, 149.0),
-(34, 93.2),
-(90, 194.0),
-(42, 107.60000000000001),
-(86, 186.8),
-(31, 87.80000000000001),
-(31, 87.80000000000001),
-(18, 64.4),
-(57, 134.60000000000002),
-(85, 185.0),
-(3, 37.4),
-(65, 149.0),
-(82, 179.6),
-(56, 132.8),
-(9, 48.2),
-(66, 150.8),
-(67, 152.60000000000002),
-(44, 111.2),
-(88, 190.4),
-(36, 96.8),
-(2, 35.6),
-(44, 111.2),
-(33, 91.4),
-(46, 114.8),
-(64, 147.2),
-(48, 118.4),
-(59, 138.2),
-(0, 32.0),
-(73, 163.4),
-(14, 57.2),
-(72, 161.6),
-(32, 89.6),
-(16, 60.8),
-(32, 89.6),
-(29, 84.2),
-(8, 46.4),
-(94, 201.20000000000002),
-(54, 129.2),
-(61, 141.8),
-(52, 125.60000000000001),
-(21, 69.80000000000001),
-(32, 89.6),
-(6, 42.8),
-(11, 51.8),
-(27, 80.6),
-(80, 176.0),
-(13, 55.400000000000006),
-(58, 136.4),
-(57, 134.60000000000002),
-(46, 114.8),
-(18, 64.4),
-(128, 262.4),
-(14, 57.2),
-(184, 363.2),
-(47, 116.60000000000001),
-(85, 185.0),
-(30, 86.0),
-(94, 201.20000000000002),
-(30, 86.0),
-(177, 350.6),
-(197, 386.6),
-(187, 368.6),
-(186, 366.8),
-(118, 244.4),
-(197, 386.6),
-(155, 311.0),
-(141, 285.8),
-(183, 361.40000000000003),
-(173, 343.40000000000003),
-(162, 323.6),
-(193, 379.40000000000003),
-(161, 321.8),
-(78, 172.4),
-(8, 46.4),
-(35, 95.0),
-(35, 95.0),
-(176, 348.8),
-(68, 154.4),
-(72, 161.6),
-(47, 116.60000000000001),
-(179, 354.2),
-(148, 298.40000000000003),
-(173, 343.40000000000003),
-(27, 80.6),
-(10, 50.0),
-(87, 188.6),
-(92, 197.6),
-(20, 68.0),
-(101, 213.8),
-(177, 350.6),
-(112, 233.6),
-(101, 213.8),
-(189, 372.2),
-(180, 356.0),
-(62, 143.60000000000002),
-(7, 44.6),
-(51, 123.8),
-(85, 185.0),
-(43, 109.4),
-(88, 190.4),
-(126, 258.8),
-(117, 242.6),
-(4, 39.2),
-(16, 60.8),
-(143, 289.40000000000003),
-(12, 53.6),
-(143, 289.40000000000003),
-(6, 42.8),
-(55, 131.0),
-(114, 237.20000000000002),
-(1, 33.8),
-(42, 107.60000000000001),
-(159, 318.2),
-(185, 365.0),
-(156, 312.8),
-(71, 159.8),
-(189, 372.2),
-(87, 188.6),
-(38, 100.4),
-(177, 350.6),
-(100, 212.0),
-(162, 323.6),
-(119, 246.20000000000002),
-(5, 41.0),
-(101, 213.8),
-(117, 242.6),
-(23, 73.4),
-(187, 368.6),
-(78, 172.4),
-(74, 165.20000000000002),
-(45, 113.0),
-(145, 293.0),
-(43, 109.4),
-(171, 339.8),
-(132, 269.6),
-(54, 129.2),
-(71, 159.8),
-(79, 174.20000000000002),
-(180, 356.0),
-(45, 113.0),
-(70, 158.0),
-(39, 102.2),
-(30, 86.0),
-(85, 185.0),
-(194, 381.2),
-(24, 75.2),
-(93, 199.4),
-(10, 50.0),
-(18, 64.4),
-]
+#Training data not included here for ease of reading, created via script 
+  # Data corelates to celcius to fareinheit conversions,
+  # Was included in a previous commit if you are interested.
+DATA = TRAINING_DATA
 
 class genetic_specimin:
   def __init__(self,second_order,first_order,constant):
-    #print("created specimin: {x} {y} {z}".format(id = specimin, x = second_order, y = first_order, z =constant))
     self.second_order = second_order
     self.first_order = first_order
     self.constant = constant
@@ -229,46 +33,86 @@ class genetic_specimin:
     
 
   def make_guess(self,cell):
-    output = (self.second_order*cell*cell) + (self.first_order*cell) + self.constant
-    #output = self.first_order*cell + self.constant
-    return output
+    return (self.second_order*cell*cell) + (self.first_order*cell) + self.constant
+    
 
-  def ask(self):
+  def ask(self):  #for debugging and visualation, allows us to see how close it came
     return (self.second_order,self.first_order,self.constant)
 
   def reproduce(self, index):
     global SPECIMINS_PER_GEN
-    global df # division factor used when making children
+    global df # division factor, increases when stagnated so needs to be global
 
     gen_score = index/SPECIMINS_PER_GEN #0-1 score relative to this generation
     abs_score = self.score/df  #absolute score (lower is better)
     scale = gen_score * abs_score  #caching for efficency and readablity
-    def child_terms(term):  #local func reduces amount to type.
+    
+    def child_terms(term, term_score):  #local func reduces amount to type.
       return term + (
-        np.random.random()  #random number 0-1
-       * np.random.choice([1,-1])  #randomly add or subtract
+        np.random.random()  #random number 0-1 (uniform)
+       * np.random.choice([1,-1])  #randomly add or subtract (50-50)
        * scale # makes change smaller the better score we get
        * abs(np.tanh(1.5*term))  #allows us to get closer to zero
+       * term_score
       )  
-      # a tansig function would be more performant
-      # a better scoring system can use score/cell**2 to scale second_order
-        # (score/cell**2)/ cell for  first order 
-        # and score % cell for const
-          # this also helps the activation function allow terms to be zero 
-
-    child_2o = child_terms(self.second_order)
-    child_1o = child_terms(self.first_order)
-    child_const = child_terms(self.constant)
+      # a tansig function is generally more performant but numpy will be faster than python code.
+    
+    
+    child_2o = child_terms(self.second_order, self.score_second_order)
+    child_1o = child_terms(self.first_order, self.score_first_order)
+    child_const = child_terms(self.constant, self.score_constant)
     
     return (child_2o, child_1o , child_const)
 
-def test_specimin(subject):
-  results = []
-  for pair in TRAINING_DATA:
-    guess = subject.make_guess(pair[0])
-    correct_answer = pair[1]
-    results.append(correct_answer - guess)
-  return results
+  def score_worst_guess(self):
+    worst_guess = 0
+    global DATA
+    for pair in DATA:
+      score_ = pair[1] - self.make_guess(pair[0])      
+      if (abs(score_) > worst_guess):
+        worst_guess = abs(score_)
+        
+    self.score_second_order, self.score_first_order, self.score_constant = 1
+    self.score = worst_guess
+    return None
+
+  def score_each_term(self):
+    global DATA
+    if self.score == None:  #avoids calulating scores twice
+      self.score_second_order = 0
+      self.score_first_order = 0
+      self.score_constant = 0
+      catch = 0
+      for pair in DATA:
+        if pair[0] == 0:
+          catch += 1
+          continue
+        # x = pair[0]
+        # y = pair[1]
+        y_minus_c_over_x = (pair[1] - self.constant)/pair[0]
+        # x2 = pair[0]**2
+        self.score_second_order += abs((
+          (1/pair[0]) * (y_minus_c_over_x - self.first_order)) - self.second_order)
+        
+        self.score_first_order += abs( 
+          y_minus_c_over_x - (self.second_order * pair[0]) -self.first_order)
+
+        self.score_constant += abs( 
+          pair[1] - (pair[0] * ((self.second_order * pair[0]) + self.first_order)) - self.constant)
+        
+      self.score_second_order /= len(DATA) - catch 
+      self.score_first_order /= len(DATA) - catch  
+      self.score_constant /= len(DATA) - catch  
+      self.score = self.score_second_order + self.score_first_order + self.score_constant
+    return None
+  def score_rms(self):
+    total = 0
+    for pair in DATA:
+      x = pair[1] - self.make_guess(pair[0])
+      total += x * x
+    self.score_second_order, self.score_first_order, self.score_constant = 1
+    self.score = np.sqrt(total) 
+    return np.sqrt(total)
 
 def initalisation(genetic_pool_l = []):
   # spawn seed specimin
@@ -277,84 +121,22 @@ def initalisation(genetic_pool_l = []):
     second_order = np.random.randint(100)
     first_order = np.random.randint(100)
     constant = np.random.randint(100)
-    genetic_pool_l.append(genetic_specimin(second_order, first_order, constant))
+    specimin = genetic_specimin(second_order, first_order, constant)
+    specimin.score = None
+    genetic_pool_l.append(specimin)
     # print("created specimin " + str(specimin) +":"+" "+second_order+","+first_order+" "+constant)
     # print("created specimin {id} : {x} {y} {z}".format(id = specimin, x = second_order, y = first_order, z =constant))
   return genetic_pool_l
 
-def  score_rms(subject):
-  total = 0
-  for x in subject.results:
-    total+=x*x
-  score = np.sqrt(total)
-  return score
-
-def score_worst_guess(subject):
-  worst_guess = subject.results[0]
-  for guess in subject.results:
-    if abs(guess) > worst_guess:
-        worst_guess = abs(guess)
-  return worst_guess
 
 
 def simulate_generation(genetic_pool_l = genetic_pool):
-  
-  scores_this_round = []
-  for subject in genetic_pool_l:
-    subject.results = test_specimin(subject)
-  
-    #score them
-    subject.score = score_worst_guess(subject) #===choose scoreing method
-    scores_this_round.append(subject.score)
-    #print("  {x}".format( x = subject.score) )
-  
-  #scores_last_round = 0
-  global generation 
-  # if 'generation' not in locals():
-  #global delta_score_avg
-  global stag_avg
-  
-  delta_score = np.average(scores_this_round) - np.average(scores_moving_avg)
-  scores_moving_avg[generation % len(scores_moving_avg)] = np.average(scores_this_round)
-  stag_avg[ generation % len(stag_avg) ] = (delta_score > 0)
-  delta_score_avg[generation % len(delta_score_avg)] = delta_score
-  
-  print("round average: {x}  d:{d}  gen: {gen}".format(
-    x= round(np.average(scores_this_round),3), 
-    d= round(delta_score, 4 ), 
-    gen= generation, 
-  )) 
-  global df
-
-  # Flipiing back and forth generally means the random scale is too high
-    # This guesses when it stagnates and increases the division factor
-    # guessing stagnation should be done with differentiation 
-  if (
-    (np.sum(stag_avg) > (0.3 * len(stag_avg))) 
-    & (abs(np.average(delta_score_avg)) < 50)
-  ):
-    df += 1 
-    stag_avg = [0] * len(stag_avg)
-      # only one to avoid random mistakes, 
-      
-    print("DivisionFactor increased")
-
-  
-  #///CULL THE UNDER PERFOMERS///
-  genetic_pool_l.sort(key=lambda x: x.score)
-  global SPECIMINS_PER_GEN
-  while len(genetic_pool_l) > SPECIMINS_PER_GEN:
-    genetic_pool_l.pop()
-  global CtoF 
-  #print("alive: {}".format(len(genetic_pool)))
-  CtoF.append(genetic_pool_l[0]) # record the best form each generation
-
   def calculate_variance(): 
     # curently unused, the idea was to use variance in reproduction
     second_orders = []
     first_orders = []
     constants = []
-    scores =[]
+    scores = []
     
     for specimin in genetic_pool:
       second_orders.append(specimin.second_order)
@@ -369,43 +151,107 @@ def simulate_generation(genetic_pool_l = genetic_pool):
     var[0] = np.std(constants)
     var[3] = np.std(scores)
     return var
+  if (len(genetic_pool_l) < 1):
+    genetic_pool_l = genetic_pool
+  avg_scores_this_round = 0
+  global SCORE_METHOD
+  for subject in genetic_pool_l:
+    #score them 
+    if SCORE_METHOD == "PER_TERM":  
+      subject.score_each_term() 
+    elif SCORE_METHOD == "RMS":
+      subject.score_rms()
+    elif SCORE_METHOD == "WORST_GUESS":
+      subject.score_worst_guess()
+    #This code doesn't look great but python doesn't have a switch and enums were less readable.
+    avg_scores_this_round += subject.score
+  avg_scores_this_round /= len(genetic_pool_l)
+
+  global generation 
+  global stag_avg
+
+
+  delta_score = avg_scores_this_round - np.average(scores_moving_avg)
+  scores_moving_avg[generation % len(scores_moving_avg)] = avg_scores_this_round
+  stag_avg[ generation % len(stag_avg) ] = (delta_score > 0)
+  delta_score_avg[generation % len(delta_score_avg)] = delta_score
   
+  print("round average: {x}  d:{d}  gen: {gen}".format(
+    x = round(avg_scores_this_round, 6), 
+    d = round(delta_score, 4 ), 
+    gen = generation, 
+  )) 
+  global df
+
+  # Flipiing back and forth generally means the random scale is too high
+    # This guesses when it stagnates and increases the division factor
+    # guessing stagnation should be done with differentiation 
+  if (
+    (np.sum(stag_avg) > (0.3 * len(stag_avg))) 
+    and (abs(np.average(delta_score_avg)) < 50)
+  ):
+    df += 1 
+    stag_avg = [0] * len(stag_avg)
+    # only one to avoid random mistakes, 
+    
+    print("DivisionFactor increased")
+
+
+  #///CULL THE UNDER PERFOMERS///
+  genetic_pool_l.sort(key=lambda x: x.score)
+  global SPECIMINS_PER_GEN
+  while (len(genetic_pool_l) > SPECIMINS_PER_GEN):
+    genetic_pool_l.pop()
   
-  
+  global CtoF 
+  CtoF.append(genetic_pool_l[0]) # record the best form each generation
   children = [] 
   for index, subject in enumerate(genetic_pool_l): # breed them
     for i in range(NUM_CHILDREN): 
+
       x = subject.reproduce(index)
-      children.append(genetic_specimin(x[0],x[1],x[2]))
-    if (subject.score < np.average(scores_this_round)): 
+      spec = genetic_specimin(x[0],x[1],x[2])
+      spec.score = None
+      children.append(spec)
+      
+    if (subject.score < avg_scores_this_round): 
       genetic_pool_l.remove(subject)  
       # if the generation is really bad keep some parents, 
         # reduces poor performace at the start
-
   
-    
-    # x = subject.reproduce(index +1)
-    # children.append(genetic_specimin(x[0],x[1],x[2]))
-    # y = subject.reproduce(index +1)
-    # children.append(genetic_specimin(y[0],y[1],y[2]))
-    # genetic_pool.remove(subject)
-  
-  genetic_pool_l+=children # let them grow up
-  #print(len(children))
+  genetic_pool_l += children # let them grow up
   generation += 1 #for debugging and visualisation, not used in code.
   return(genetic_pool_l)
 
 def generations(n, genetic_pool_l = genetic_pool):
-  for i in range(n):
-    genetic_pool_l = simulate_generation(genetic_pool_l)
+  global generation    
+  def init_pool(genetic_pool_l):  
+    global CtoF
+    for i in range(25):
+      genetic_pool_l = simulate_generation(genetic_pool_l)  
+      
+    if (CtoF[-1].score >= ((CtoF[0].score)/1.5)):
+      global generation
+      print("bad seed, resetting ")
+      print( CtoF[-1].score, CtoF[0].score)
+      CtoF = []
+      genetic_pool_l = initalisation()
+      generation = 0
+      init_pool(genetic_pool_l)
+  if (generation < 25) and (n > 25):
+    init_pool(genetic_pool_l)
+    for i in range( (n - 25) ):
+      genetic_pool_l = simulate_generation(genetic_pool_l)  
+  else:
+    for i in range(n):
+      genetic_pool_l = simulate_generation(genetic_pool_l)
   return genetic_pool_l
 
-
-
-def main(n = 50, genetic_pool_local = genetic_pool):  
+def main(genetic_pool_local = genetic_pool , n = 50):  
   genetic_pool_local = generations(n, genetic_pool_local)
-  print("simulation done")  
+  print("simulation done") 
+  
   return genetic_pool_local
 
 if __name__ == "__main__" :
-  My_genetic_pool = main(50, initalisation())
+  genetic_pool = main(initalisation(), 30)
