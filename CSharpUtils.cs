1454a81c985da1dec1918d82a14bd48cc1264a7f
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2022-08-10 22:36:30 +0100

    Added XML style documentation

--- CSharpUtils.cs
+++ CSharpUtils.cs
@@ -1,28 +1,37 @@
 //Edit the file name to something more relevent, like utils
 using System;
 
-namespace EDIT_ME //change to relevent namespace
+namespace EDIT_ME // change to something more relevent for the application
 {
   public static class Extentions 
   {
-    
-    public static string Times(this string str, int imax)
+		///<summary> acts like pyhton's str * int functions</summary>
+		///<param name= "str"> the string to multiply</param>
+		///<param name = "n"> the amount of times to multiply the string</param>
+		///<returns> the string n times with no spaces or linebreaks</returns>
+    public static string Times(this string str, int n)
     {
       string sb = "";
-      for(int i = 0; i < imax; i++ )
+      for(int i = 0; i < n; i++ )
       {
         sb += str;
       }
       return sb;
     }
   }
-  
+
   public static class MyMath  //cannot extend from "math" :(
   {
+		///<summary> Sigmoid activation function</summary> 
+		///<param name = "f" > a double of any value </param>
+		///<returns> a floating point double between 0 and 1 </returns>
     public static double Sigmoid(double f)
     {
       return 1/(1 + Math.Pow(Math.E,-f));
     }
+		///<summary> TanSig activation function</summary> 
+		///<param name = "f" > a double of any value </param>
+		///<returns> a floating point double between -1 and 1 </returns>
     public static double Tansig(double f)
     {
       return (2/(1 + Math.Pow(Math.E,-2*f))) -1;
